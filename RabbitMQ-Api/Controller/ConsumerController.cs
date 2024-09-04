24dc6330cc1ecb4e09e0023b99191d7ae2c9fdd8
envolti.ferrari <envolti.ferrari@softplan.com.br>
2024-09-04 15:38:46 -0300

    inclusão de web.api para testes.

---
+++ RabbitMQ-Api/Controller/ConsumerController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMq;
+using RabbitMQ_Api.Consumer;
+
+namespace RabbitMQ_Api.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsumerController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            await Task.CompletedTask;
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromServices] PublisherConsumer publisher, [FromBody] ConsumerModel body)
+        {
+            try
+            {
+                await publisher.Publish(body, null, CancellationToken.None);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
